7c4c61b352e5b6b2a4f958ea807f038be71676f4
Abhinav Vedati <52864339+Avedati@users.noreply.github.com>
2019-09-16 08:56:55 -0700

    Update lexer.py

--- lexer.py
+++ lexer.py
@@ -1,35 +1,138 @@
+"""
+
+ class Token
+
+ This is a class that representes a single token lexed by the tokenizer. It has two fields, a type and a lexeme.
+
+"""
+
 class Token:
 
+	"""
+	
+	 Token.__init__(self, type_, lexeme)
+	 
+	 This function is called whenever a new token is created.
+	 
+	 @param self This instance of the token class.
+	 @param type_ The type of the token.
+	 @param lexeme The lexeme of the token (the string of characters that was consumed by the tokenizer).
+	
+	"""
+	
 	def __init__(self, type_, lexeme):
-		self.type_ = type_
-		self.lexeme = lexeme
+		self.setType(type_)
+		self.setLexeme(lexeme)
 
+	"""
+	
+	 Token.__str__(self)
+	 
+	 This function is called whenever the Token object is converted to a string (for example, when it is printed).
+	 
+	 @param self This instance of the Token class.
+	 @return A string representation of this token.
+	
+	"""
+		
 	def __str__(self):
 		return '< ' + self.getType() + ', ' + self.getLexeme() + ' >'
 
+	"""
+	
+	 Token.getType(self)
+	 
+	 This is a safe getter method for the type_ field of the Token class.
+	 
+	 @param self This instance of the Token cass.
+	 @return The type_ field.
+	
+	"""
+	
 	def getType(self):
 		return self.type_
-
+	
+	"""
+	
+	 Token.getLexeme(self)
+	 
+	 This is a safe getter method for the lexeme field of the Token class.
+	 
+	 @param self This instance of the Token cass.
+	 @return The lexeme field.
+	
+	"""
+	
 	def getLexeme(self):
 		return self.lexeme
 
+	"""
+	
+	 Token.setType(self, type_)
+	 
+	 This is a safe setter method for the type_ field of the Token class.
+	 
+	 @param self This instance of the Token class.
+	
+	"""
+	
 	def setType(self, type_):
 		self.type_ = type_
 
+	"""
+	
+	 Token.setLexeme(self, lexeme)
+	 
+	 This is a safe setter method for the lexeme field of the Token class.
+	 
+	 @param self This instance of the Token class.
+	
+	"""
+		
 	def setLexeme(self, lexeme):
 		self.lexeme = lexeme
 
+"""
+
+ class Tokenizer
+
+ This is a class that handles lexing an input string, turning it into the appropriate tokens for the Stork programming language.
+
+"""
 class Tokenizer:
 
-	ATOM_OPERATORS = ['+', '-']
-	EXPR_OPERATORS = ['*', '/', '%']
-	TOKENS = ['=']
-	PUNC = ['(',')']
+	ATOM_OPERATORS = ['+', '-'] # The valid operators that can act on a base (a number, string, variable, or function call).
+	EXPR_OPERATORS = ['*', '/', '%'] # The valid operators that can act on an atom (an base + or - a base).
+	TOKENS = ['='] # Other tokens
+	PUNC = ['(',')'] # Punctuation marks
 
+	"""
+	
+	 Tokenizer.__init__(self, source)
+	 
+	 This function is called when a new Tokenizer object is created.
+	 
+	 @param self This instance of the Tokenizer class.
+	 @param source The source string to be interpreted by the Stork interpreter.
+	
+	"""
+	
 	def __init__(self, source):
 		self.source = source
 		self.pos = 0
 
+	"""
+	
+	 Tokenizer.advance_while(self, test)
+	 
+	 This function will consume characters from the source string as long as they return True based on a test function.
+	 
+	 @param self This instance of the Tokenizer class.
+	 @param test A function that takes a character and returns either True or False.
+	 @return The characters that were read and consumed.
+	
+	"""
+		
 	def advance_while(self, test):
 		lexeme = ''
 		while not self.end() and test(self.source[self.pos]):
@@ -37,9 +140,31 @@ class Tokenizer:
 			self.pos += 1
 		return lexeme
 
+	"""
+	
+	 Tokenizer.end(self)
+	 
+	 This function returns True if the current position of the tokenizer is at or past the end of the source string.
+	 
+	 @param self This instance of the Tokenizer class.
+	 @return Whether or not our tokenizer's current position is outside of the source string.
+	
+	"""
+	
 	def end(self):
 		return self.pos >= len(self.source)
 
+	"""
+	
+	 Tokenizer.tokenize(self)
+	 
+	 This function will tokenize the source string (converting it into a list of tokens).
+	 
+	 @param self This instance of the Tokenizer class.
+	 @return An array of tokens.
+	
+	"""
+	
 	def tokenize(self):
 		tokens = []
 		while not self.end():
